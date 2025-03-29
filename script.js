42b3e83740fee612db2d67c8b4a1e900e235840c
collabphp <aliag-bcf203@code.edu.az>
2025-03-29 12:58:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.getElementById('uploadForm').addEventListener('submit', async (event) =
         const repoOwner = 'collabphp'; // Your GitHub username
         const repoName = 'collab.github.io'; // Your repository name
         const branch = 'root'; // Your branch name
-        const token = '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'; // Replace with your new GitHub Personal Access Token
+        const token = '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'; // Replace with your new GitHub Personal Access Token
 
         const url = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/uploads/${fileName}`;
 
